be87195cee8ec7da9cf7ae5f0017b9f129540a02
Ahmed Noori <moe7ca@gmail.com>
2022-04-20 15:16:33 -0400

    B

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-// import logo from './logo.svg';
-import './App.css';
-import Speech from './components/Speech';
-import Voice from './components/Voice';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.css';
 
 function App() {
   return (
     <div className="App">
-      <Speech />
-      <Voice />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
+
 export default App;
