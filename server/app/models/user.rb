8c359c85ed5fb457e4cfdae51a50ca76b69e5252
Amro Hassan <79529406+hass0319@users.noreply.github.com>
2022-04-23 23:20:40 -0400

    refactoring validate

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,11 +1,7 @@
-class User < ApplicationRecord
   class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: true
-    validates :username, uniqueness: true
-    validates :username, length: { minimum: 4 }
+    validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true ,length: { minimum: 3 }
     validates :password_confirmation, presence: true, length: { minimum: 3 }
-end
 end
