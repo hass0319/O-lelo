ecaf27b8b4af308d6f7390020c596f532995c969
Amro Hassan <79529406+hass0319@users.noreply.github.com>
2022-04-23 23:15:35 -0400

    adds voice page

--- client/src/components/Voice.js
+++ client/src/components/Voice.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
-import "../styles/Voice.scss";
+import "./Voice.scss";
 
 function Voice() {
 
