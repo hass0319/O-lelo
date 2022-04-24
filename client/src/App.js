375e7625768a5e2a74b7e8bc7528c5806ce3b12c
Amro Hassan <79529406+hass0319@users.noreply.github.com>
2022-04-23 23:18:12 -0400

    adds browser Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,68 @@
 // import logo from './logo.svg';
-import React, { Component } from 'react';
-import axios from 'axios'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css';
-import Speech from './components/Speech';
+import axios from 'axios'
+import Home from './components/Home';
 import Voice from './components/Voice';
+import Login from './components/login';
+import Speech from './components/Speech';
+import Signup from './components/signup';
+import { useState, useEffect } from 'react';
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-function App({ props }) {
+
+function App() {
+  const [state, setState] = useState({
+    isLoggedIn: false,
+    user: {}
+  });
+
+  useEffect(() => {
+    const loginStatus = () => {
+      axios.get('/logged_in',
+        { withCredentials: true })
+        .then(res => {
+          if (res.data.logged_in) {
+            handleLogin(res)
+          } else {
+            handleLogout()
+          }
+        })
+        .catch(error => console.log('api errors:', error))
+    };
+  }, []);
+
+  const handleLogin = (data) => {
+    setState({
+      isLoggedIn: true,
+      user: data.user
+    });
+  }
+
+  const handleLogout = () => {
+    setState({
+      isLoggedIn: false,
+      user: {}
+    });
+  }
+
   return (
-    <div className="App">
-      <Speech />
-      <Voice />
+    <div>
+      <Router>
+        <Routes>
+
+          <Route exact path='/' element={<Home />} />
+
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/signup' element={<Signup />} />
+
+          <Route exact path='/voice' element={<Voice />} />
+          <Route exact path='/speech' element={<Speech />} />
+
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
+
 export default App;
