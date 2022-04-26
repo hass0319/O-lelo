d65bc56728b623a873b817cd21eaab0c37bc4318
Amro Hassan <79529406+hass0319@users.noreply.github.com>
2022-04-26 14:39:27 -0400

    renames speech file, from voice... and adds css

--- client/src/components/Speech.js
+++ client/src/components/Speech.js
@@ -1,34 +1,98 @@
-import { useState } from "react";
-import { useSpeechSynthesis } from "react-speech-kit";
+import { useRef, useState } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 import "./Speech.scss";
 
-const Speech = () => {
-  const [value, setValue] = useState("");
-  const { speak } = useSpeechSynthesis();
+export default function Speech() {
 
-  console.log("value => " + value);
-  console.log("setValue => " + setValue);
-  console.log("spesch => " + speak);
-  console.log("useSpeechSynthesis => " + useSpeechSynthesis);
+  const commands = [
+    {
+      command: "open *",
+      callback: (website) => {
+        window.open("http://" + website.split(" ").join(""));
+      }
+    },
+    {
+      command: "change background colour to *",
+      callback: (color) => {
+        document.body.style.background = color;
+      }
+    },
+    {
+      command: "reset",
+      callback: () => {
+        handleReset();
+      }
+    },
+    {
+      command: "reset background colour",
+      callback: () => {
+        document.body.style.background = `rgba(0, 0, 0, 0.8)`;
+      }
+    }
+  ];
+
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
+  const [isListening, setIsListening] = useState(false);
+  const microphoneRef = useRef(null);
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return (
+      <div className="mircophone-container">
+        Browser is not Support Speech Recognition.
+      </div>
+    );
+  }
+
+  const handleListing = () => {
+    setIsListening(true);
+    microphoneRef.current.classList.add("listening");
+    SpeechRecognition.startListening({
+      continuous: true,
+    });
+  };
+
+  const stopHandle = () => {
+    setIsListening(false);
+    microphoneRef.current.classList.remove("listening");
+    SpeechRecognition.stopListening();
+  };
+
+  const handleReset = () => {
+    stopHandle();
+    resetTranscript();
+  };
 
   return (
-    <div className="speech">
-      <div className="group">
+
+    <div className="microphone-wrapper">
+      <div className="title">
         <h2>Text To Speech Converter</h2>
       </div>
-      <div className="group">
-        <textarea
-          rows="10"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        ></textarea>
+      <div className="mircophone-container">
+        <div
+          className="microphone-icon-container"
+          ref={microphoneRef}
+          onClick={handleListing}
+        >
+          🎙{/* <img src={"🎙"} alt="microPhoneIcon" className="microphone-icon" /> */}
+        </div>
+        <div className="microphone-status">
+          {isListening ? "Listening........." : "Click to start Listening"}
+        </div>
+        {isListening && (
+          <button className="microphone-stop btn" onClick={stopHandle}>
+            Stop
+          </button>
+        )}
       </div>
-      <div className="group">
-        <button onClick={() => speak({ text: value })}>
-          Speech
-        </button>
-      </div>
+      {transcript && (
+        <div className="microphone-result-container">
+          <div className="microphone-result-text">{transcript}</div>
+          <button className="microphone-reset btn" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      )}
     </div>
   );
-};
-export default Speech;
+}
