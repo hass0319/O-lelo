b39ab77bc16fc7fdfe77044e8af01e345ea9927e
Amro Hassan <79529406+hass0319@users.noreply.github.com>
2022-04-26 14:37:25 -0400

    edits home page and adds css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import Button from './Button';
+import "./Home.scss";
 
-function Home() {
+export default function Home() {
   return (
     <div>
-      <Link to='/login'>Log In</Link>
-      <br></br>
-      <Link to='/signup'>Sign Up</Link>
+      <div className="open_text">
+        <h1>ŌLELO  قوليلو</h1>
+        <h1>An app that<br /> makes life<br />Eassier</h1>
+      </div >
+      <div className="text_bttn">
+        <Button href="/text">Write to Me</Button>
+        <p>Write and I'll be your voice</p>
+      </div>
+      <div className="speech_bttn">
+        <Button>Talk to Me</Button>
+        <p>Talk and I'll do the Writing</p>
+        <h5>SignUp and your command is my</h5>
+        <h3>Duty</h3>
+      </div>
     </div>
+
   );
 }
-export default Home;
